c6566a7874ba9f96d45ded04f815f082bbc452d9
shray0027 <59202921+shray0027@users.noreply.github.com>
2021-01-28 00:10:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine' ,'ejs' );
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended :true}));
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+let port  = process.env.PORT;
+if(port ===null || port===""){
+    port=3000;
+}
+app.listen(3000,()=>{
+    console.log("site hosted on port 3000");
+})
---
+++ public/js/index.js
@@ -0,0 +1,55 @@
+const year = document.querySelector(".current-year");
+const rn = document.querySelector(".rn-content");
+const arr = document.querySelector(".arr-content");
+const t = document.querySelector(".t-content");
+const g = document.querySelector(".g-content");
+const rn_l = document.querySelector(".rn-label");
+const arr_l = document.querySelector(".arr-label");
+const t_l = document.querySelector(".t-label");
+const g_l = document.querySelector(".g-label");
+const date =  new Date();
+year.textContent = date.getFullYear();
+const choose_rn = ()=>{
+    rn.style.display="block";
+    rn.style.zIndex="1";
+    arr.style.display="none";
+    t.style.display="none";
+    g.style.display="none";
+    rn_l.style.zIndex="2";
+    arr_l.style.zIndex="0";
+    t_l.style.zIndex="0";
+    g_l.style.zIndex="0";
+}
+const choose_arr = ()=>{
+    arr.style.display="block";
+    arr.style.zIndex="1";
+    rn.style.display="none";
+    t.style.display="none";
+    g.style.display="none";
+    rn_l.style.zIndex="0";
+    arr_l.style.zIndex="2";
+    t_l.style.zIndex="0";
+    g_l.style.zIndex="0";
+}
+const choose_t = ()=>{
+    t.style.display="block";
+    t.style.zIndex="1";
+    arr.style.display="none";
+    rn.style.display="none";
+    g.style.display="none";
+    rn_l.style.zIndex="0";
+    arr_l.style.zIndex="0";
+    t_l.style.zIndex="2";
+    g_l.style.zIndex="0";
+}
+const choose_g = ()=>{
+    g.style.display="block";
+    g.style.zIndex="1";
+    arr.style.display="none";
+    t.style.display="none";
+    rn.style.display="none";
+    rn_l.style.zIndex="0";
+    arr_l.style.zIndex="0";
+    t_l.style.zIndex="0";
+    g_l.style.zIndex="2";
+}
